563c8f0d79f1db5937e09a554a121c951446bb5f
Zagraxe <oussamdin123@gmail.com>
2025-04-04 04:06:50 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,19 @@ import os
 import requests
 import base64
 
+# Get environment variable
 TOKEN = os.getenv("DISCORD_TOKEN")
-BANNER_PATH = "assets/IMG_20250404_030139.png"
+BANNER_PATH = "IMG_20250404_030139.png"  # Image located in the root directory
 
+# Read and encode image
 with open(BANNER_PATH, "rb") as image_file:
     encoded_data = base64.b64encode(image_file.read()).decode('utf-8')
 
+# Format as data URI
 data_url = f"data:image/png;base64,{encoded_data}"
 url = "https://discord.com/api/v10/users/@me"
 
+# Send PATCH request to update bot banner
 response = requests.patch(
     url,
     headers={
